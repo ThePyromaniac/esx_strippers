ab6251f5412de07cec0fc2ca87dc11d887d0a570
NEIDER12 <36943294+NEIDER12@users.noreply.github.com>
2020-12-26 22:04:48 -0600

    Fixed Strippers
    Fixed the strippers, added a couple more to fit my needs, just change positions on the config.lua, also the Z position needs to be at least 0.50 HIGHER to get them to the actual floor

--- server.lua
+++ server.lua
@@ -1,10 +1,1 @@
 TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
-local spawned = false
-
-ESX.RegisterServerCallback('callbackname', function( source, cb )
-	cb(spawned)
-    if not spawned then
-        spawned = true
-    end
-
-end)
